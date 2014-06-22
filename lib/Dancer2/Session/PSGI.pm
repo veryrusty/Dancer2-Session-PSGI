d61e9a22bb64f99e83d646fe0a87d3b0c3d2c649
Russell Jenkins <veryrusty@gmail.com>
2014-06-22 22:13:40 +1000

    Pass upstream any changes to cookie expiry that an app may have altered.

--- lib/Dancer2/Session/PSGI.pm
+++ lib/Dancer2/Session/PSGI.pm
@@ -41,12 +41,19 @@ sub _sessions { return [] }
 #-----------------------------------------#
 
 # Middleware sets the cookie.
-# Set options.expire if cookie has expired, e.g. result of delete_session.
 sub set_cookie_header {
     my ( $self, %params ) = @_;
+
     my $session = $params{session};
-    if ( $session->expires && $session->expires < time ) {
-        $self->context->env->{'psgix.session.options'}{expire} = 1;
+    my $options = $self->context->env->{'psgix.session.options'};
+
+    if ( my $expires = $session->expires ) {
+        if ( $session->expires < time ) { # Cookie has expired.
+            $options->{expire} = 1;
+        }
+        else { # Pass upstream the cookie expire time
+            $options->{expires} = $expires;
+        }
     }
 }
 
